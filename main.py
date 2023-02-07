553822431950d28ad1c6ba415528274dad59f0e6
nammonman <sirapat69@outlook.com>
2023-02-07 15:03:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException, Body
-from datetime import date
+from datetime import datetime
 from pydantic import BaseModel
 from pymongo import MongoClient
 
@@ -11,9 +11,9 @@ MONGO_DB_PORT = 8443
 
 class Management(BaseModel):
     available: bool
-    locker_id : int
-    start_time: date
-    end_time: date
+    locker_id: int
+    start_time: datetime
+    end_time: datetime
     items: list
     user_id: int
 
@@ -35,31 +35,35 @@ def locker_avaliable(locker_id: int, status: bool):
         raise HTTPException(status_code=404, detail=f"No locker available right now")
     return lockers
 
+
 @app.post("/reserve_locker")
 def reserve(management: Management):
-    start_time = management.start_time.strftime("%Y-%m-%d")
-    end_time = management.end_time.strftime("%Y-%m-%d")
+    start_time = management.start_time
+    end_time = management.end_time
     l_id = management.locker_id
-    if (start_time > end_time):
+    if start_time > end_time:
         raise HTTPException(status_code=400, detail="Reservation can not be made")
-    if (l_id < 0 or l_id > 6):
+    if l_id < 1 or l_id > 6:
         raise HTTPException(status_code=400, detail="Reservation can not be made")
     query = {
         "available": bool(management.available),
-        "locker_id" : l_id,
+        "locker_id": l_id,
         "start_time": start_time,
         "end_time": end_time,
         "items": management.items,
         "user_id": int(management.user_id)
     }
-    collection.update_one(query,{f"$set":query},upsert=True)
+    collection.update_one(query, {f"$set": query}, upsert=True)
     return {
         "available": bool(management.available),
-        "locker_id" : l_id,
-        "start_time":start_time,
+        "locker_id": l_id,
+        "start_time": start_time,
         "end_time": end_time,
         "items": management.items,
         "user_id": management.user_id
     }
 
-
+@app.delete("/reserve_locker/delete/{locker_id}/{start_time}")
+def delete_reservation(locker_id: int, start_time: datetime):
+    collection.delete_one({"locker_id": locker_id, "start_time": start_time})
+    return "removed a reservation from locker " + str(locker_id)
